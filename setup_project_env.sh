0db247dadfe90a40eb2aa8657060026f9eb781e4
Jamie R. Oaks <joaks1@gmail.com>
2020-05-29 16:03:33 -0500

    Adding script to setup project environment.
    Adding Project.toml and setup_project_env.sh. The latter will install a
    specific commit of ecoevolity and set up a julia environment according
    to the info in Project.toml.

---
+++ setup_project_env.sh
@@ -0,0 +1,27 @@
+#! /bin/bash
+
+set -e
+
+ecoevolity_commit="508f2ea3"
+
+# Get path to directory of this script
+project_dir="$( cd -P "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+# Load modules
+echo "Loading modules..."
+source ./modules-to-load.sh >/dev/null 2>&1 || echo "    No modules loaded"
+
+echo "Setting up julia environment..."
+julia -e 'import Pkg; Pkg.activate("."); Pkg.instantiate(); Pkg.precompile(); Pkg.status()'
+
+# echo "Cloning and building ecoevolity..."
+# git clone https://github.com/phyletica/ecoevolity.git
+# (
+#     cd ecoevolity
+#     git checkout -b testing "$ecoevolity_commit"
+#     ./build.sh --prefix "$project_dir"
+#     echo "    Commit $ecoevolity_commit of ecoevolity successfully built and installed"
+# )
+# echo "Ecoevolity was successfully installed locally"
+# echo "You can now remove the ecoevolity directory using the command:"
+# echo "    rm -rf ecoevolity"
