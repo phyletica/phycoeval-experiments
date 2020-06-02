fe6c82bbd3fd8d9e069f0eaa5d7cfcabe2162b27
Jamie R. Oaks <joaks1@gmail.com>
2020-06-02 16:26:14 -0500

    Updating create_batch_of_simphycoeval_scripts.sh.
    Increasing number of reps and topo moves per rep.

--- scripts/create_batch_of_simphycoeval_scripts.sh
+++ scripts/create_batch_of_simphycoeval_scripts.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-julia --project create_batch_of_simphycoeval_scripts.jl -n 2 -t 1000 \
+julia --project create_batch_of_simphycoeval_scripts.jl -n 10 -t 1000 \
     -p ../configs/species-9-genomes-2-bifurcating-tree-random.yml \
     -p ../configs/species-9-genomes-2-generalized-tree-random.yml \
     -f ../configs/species-9-genomes-2-bifurcating-tree-provided.yml \
