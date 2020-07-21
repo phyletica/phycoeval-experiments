ec4e9eea5cf14bafebabd582cbbe90aea4ff11f5
Jamie R. Oaks <joaks1@gmail.com>
2020-07-20 22:08:56 -0500

    Updating commit SHA for ecoevolity.
    Updating the commit for ecoevolity installed by setup_project_env.sh.

--- setup_project_env.sh
+++ setup_project_env.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-ecoevolity_commit="bea7b419"
+ecoevolity_commit="a6bd852f"
 
 # Get path to directory of this script
 project_dir="$( cd -P "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
