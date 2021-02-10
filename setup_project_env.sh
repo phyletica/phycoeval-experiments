fe8c0c4d6635f4a9be5cdaf029972579d2205ca8
Jamie R. Oaks <joaks1@gmail.com>
2021-02-09 20:48:10 -0600

    Updating ecoevolity commit in setup script.
    Updating the version of ecoevolity that is installed by
    'setup_project_env.sh'.
    
    Updating for the --multiplier option in sumphycoeval.

--- setup_project_env.sh
+++ setup_project_env.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-ecoevolity_commit="d405aa64"
+ecoevolity_commit="079eb1b5"
 
 # Get path to directory of this script
 project_dir="$( cd -P "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
