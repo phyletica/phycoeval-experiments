945dc0cdef5dbba2a416a19855897882a19c4b0a
Jamie R. Oaks <joaks1@gmail.com>
2021-02-18 16:14:29 -0600

    Adding scripts for running phycoeval on geckos
    Adding scripts to sample from the prior distribution.

---
+++ scripts/gekkonid-scripts/gekko-nodata-run-01.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+set -e
+
+run=01
+nthreads=1
+config_file_prefix="gekko-nopoly"
+
+if [ -n "$PBS_JOBNAME" ]
+then
+    if [ "$nthreads" != "$PBS_NUM_PPN" ] 
+    then
+        echo "ERROR: nthreads and PBS_NUM_PPN do not match!"
+        exit 1
+    fi
+    if [ -f "${PBS_O_HOME}/.bashrc" ]
+    then
+        source "${PBS_O_HOME}/.bashrc"
+    fi
+    cd $PBS_O_WORKDIR
+fi
+
+project_dir="../.."
+exe_path="${project_dir}/bin/phycoeval"
+
+if [ ! -x "$exe_path" ]
+then
+    echo "ERROR: No executable '${exe_path}'."
+    echo "       You probably need to run the project setup script."
+    exit 1
+fi
+
+source "${project_dir}/modules-to-load.sh" >/dev/null 2>&1 || echo "    No modules loaded"
+
+config_dir="${project_dir}/configs"
+output_dir="${project_dir}/gekkonid-output"
+
+if [ ! -e "$output_dir" ]
+then
+    mkdir "$output_dir"
+fi
+
+prefix="${output_dir}/run-${run}-threads-${nthreads}-"
+config_path="${config_dir}/${config_file_prefix}.yml"
+out_path="${prefix}${config_file_prefix}.out"
+
+"$exe_path" --ignore-data --seed $run --nthreads "$nthreads" --prefix $prefix --relax-triallelic-sites --relax-missing-sites "$config_path" 1>"$out_path" 2>&1
+
