fb402753c69727c10b84fdbadf1db9bccd6a6296
Jamie R. Oaks <joaks1@gmail.com>
2021-03-04 10:45:23 -0600

    Working on gecko plotting script.

--- scripts/plot_gecko_results.jl
+++ scripts/plot_gecko_results.jl
@@ -1,7 +1,7 @@
 #! /usr/bin/env julia
 
-using YAML
 using Plots
+using StatsPlots
 
 pgfplotsx()
 default(tex_output_standalone = true)
@@ -137,22 +137,119 @@ function process_tex(path::String;
     return cropped_pdf_path
 end
 
+function parse_number_of_divs(
+        state_log_paths::String...;
+        skip::Int = 0)::Vector{Int}
+    df = ProjectUtil.get_data_frame_from_gz(collect(state_log_paths);
+            skip = skip)
+    return df[:, :number_of_heights]
+end
+
+function parse_number_of_tips(
+        state_log_path::String)::Int
+    df = ProjectUtil.get_data_frame_from_gz([state_log_path])
+    num_tips = 0
+    for col_name in names(df)
+        if startswith(col_name, "pop_size_") & (col_name != "pop_size_root")
+            num_tips += 1
+        end
+    end
+    return num_tips
+end
+
+function get_number_of_div_probs(
+        state_log_paths::String...;
+        skip::Int = 0)::Vector{Float64}
+    num_divs = Vector{Int}()
+    num_paths = 0
+    num_tips = -1
+    for p in state_log_paths
+        write(stdout, "Parsing numbers of divs from $(p)...\n")
+        ndivs = parse_number_of_divs(p; skip = skip)
+        write(stdout, "Parsed $(length(ndivs)) samples after ignoring first $(skip).\n")
+        append!(num_divs, ndivs)
+        num_paths += 1
+        if num_tips < 0
+            num_tips = parse_number_of_tips(p)
+        end
+    end
+
+    nsamples = length(num_divs)
+    write(stdout, "\nParsed a total of $(nsamples) samples from $(num_paths) files.\n")
+    write(stdout, "Number of tips: $(num_tips)\n")
+
+    @assert num_tips > 0
+    max_num_divs = num_tips - 1
+    num_div_counts = zeros(Int, max_num_divs)
+    for n in num_divs
+        num_div_counts[n] += 1
+    end
+    num_div_freqs = zeros(Float64, length(num_div_counts))
+    for i in 1:length(num_div_counts)
+        num_div_freqs[i] = num_div_counts[i] / nsamples
+    end
+
+    return num_div_freqs
+end
+
 
 function main_cli()::Cint
     write(stdout, "Plotting backend: $(backend())\n")
 
     taxa = ["gekko", "cyrt"]
+    burnin = 101
     for taxon in taxa
-        yml_path = joinpath(ProjectUtil.GEK_OUT_DIR, "posterior-summary-$(taxon)-nopoly.yml")
-        d = YAML.load_file(yml_path)
-        max_num_divs = length(d["leaf_label_map"]) - 1
-        num_div_freqs = zeros(Float64, max_num_divs)
-        for num_height_entry in d["numbers_of_heights"]
-            num_heights::Int64 = num_height_entry["number_of_heights"]
-            freq::Float64 = num_height_entry["frequency"]
-            num_div_freqs[num_heights] = freq
+        state_log_path_iter = ProjectUtil.flat_file_path_iter(
+                ProjectUtil.GEK_OUT_DIR,
+                r"^run-\d+-threads-\d+-" * taxon * r"-nopoly-state-run-1.log.gz$")
+        num_div_freqs = get_number_of_div_probs(state_log_path_iter...;
+                skip = burnin)
+        max_num_divs = length(num_div_freqs)
+
+        have_prior_samples = false
+        prior_state_log_path_iter = ProjectUtil.flat_file_path_iter(
+                ProjectUtil.GEK_OUT_DIR,
+                r"^nodata-run-\d+-threads-\d+-" * taxon * r"-nopoly-long-state-run-1.log.gz$")
+        if isready(prior_state_log_path_iter)
+            have_prior_samples = true
+            prior_num_div_freqs = get_number_of_div_probs(
+                    prior_state_log_path_iter...;
+                    skip = burnin)
+            @assert length(prior_num_div_freqs) == length(num_div_freqs)
+
+            x_ticks = collect(1:max_num_divs)
+            x_tick_labels = string.(1:max_num_divs)
+            for i in 1:max_num_divs
+                if i % 2 == 0
+                    x_tick_labels[i] = ""
+                end
+            end
+            groups = repeat(["Prior", "Posterior"], inner = max_num_divs)
+
+            p = StatsPlots.groupedbar(vcat(x_ticks, x_ticks),
+                    hcat(prior_num_div_freqs, num_div_freqs),
+                    group = groups,
+                    xlabel = "Number of divergences",
+                    ylabel = "Posterior probability",
+                    xticks = (1:max_num_divs, x_tick_labels),
+                    linewidth = 0.0,
+                    linealpha = 0.0,
+                    legend = :topright
+                    #= framestyle = :box =#
+                   )
+            Plots.plot!(p, size = (600, 280))
+            plot_path = joinpath(ProjectUtil.GEK_OUT_DIR, "number-of-divs-$(taxon).tex")
+            Plots.savefig(p, plot_path)
+            pdf_path = process_tex(plot_path, target = axis_pattern, replacement = axis_replace)
+            continue
         end
 
+        x_tick_labels = string.(1:max_num_divs)
+        for i in 1:max_num_divs
+            if i % 2 == 0
+                x_tick_labels[i] = ""
+            end
+        end
         p = Plots.bar(num_div_freqs,
                       fillcolor = gen_col,
                       linecolor = gen_col,
@@ -160,7 +257,8 @@ function main_cli()::Cint
                       linealpha = 0.0,
                       xlabel = "Number of divergences",
                       ylabel = "Posterior probability",
-                      #= yticks = (1:max_num_divs, string.(1:max_num_divs)), =#
+                      #= xticks = (1:max_num_divs, string.(1:max_num_divs)), =#
+                      xticks = (1:max_num_divs, x_tick_labels),
                       legend = false)
         Plots.plot!(p, size = (400, 280))
         plot_path = joinpath(ProjectUtil.GEK_OUT_DIR, "number-of-divs-$(taxon).tex")
